0bbe25d9901473b6c7e1b15aa0625d331fd06094
EasyBizz <randy.heijstek@gmail.com>
2025-12-20 18:18:24 +0100

    added const.py

---
+++ custom_components/peblar_ws/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "peblar_ws"
+
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+WS_PATH = "/api/v1/ws"
+LOGIN_PATH = "/api/v1/auth/login"
+
+TOPIC_SESSION = "/session/status"
+TOPIC_NETWORK = "/network/status"
